7cb789b907e349306f764d4f8dec40e7376534ef
denis.zagorulko <denys_zagorulko@knu.ua>
2023-05-24 17:19:20 +0300

    add alt

--- script/script.js
+++ script/script.js
@@ -2,10 +2,10 @@ let main_button = document.getElementById("main_button");
 let final_result = document.querySelector(".final_result");
 main_button.onclick = async function getZeroes() {
     const userFunction = encodeURIComponent(document.getElementById("function").value);
-    const response = await fetch("https://newton.vercel.app/api/v2/zeroes/" + userFunction);
-    const zeroes = await response.json();
+    const request = await fetch("https://newton.vercel.app/api/v2/zeroes/" + userFunction);
+    const reply = await request.json();
     let result = "Нулі функції: ";
-    zeroes.result.forEach((element) => {
+    reply.result.forEach((element) => {
         result += element + "; ";
     });
     final_result.innerHTML = result;
